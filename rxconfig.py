7bd061f2d47829df6fae4e85fe0364b696c9edd4
jeremy <jeremy.f.medlin@gmail.com>
2023-11-14 17:25:43 -0500

    Config

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="nursereports",
-    api_url='http://nursegrades.org:8000'
+    api_url='http://nursegrades.org:8000',
+    frontend_port=80,
 )
