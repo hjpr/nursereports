9dbd5495b3fa2e35b31c0655ce760ba33344693b
hjpr <jeremy.f.medlin@gmail.com>
2024-03-19 20:28:45 -0400

    Changed comments.

--- nursereports/nursereports.py
+++ nursereports/nursereports.py
@@ -1,13 +1,12 @@
 
 from .client.pages import *
-from .tests.pages import *
+# from .tests.pages import *
 
 
 from .server.middleware.middleware import LoggingMiddleware
 
 import reflex as rx
 
-# INITIALIZE THE APP
 app = rx.App(
     middleware=[LoggingMiddleware()],
     theme=rx.theme(
